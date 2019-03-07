e6e004ee13c6baf95b197fed43c1c0c7eca3d50c
Jeffery <jeffery124@gmail.com>
2019-03-06 20:58:54 -0500

    changed h3 to h4

--- src/update-log.js
+++ src/update-log.js
@@ -30,10 +30,10 @@ class UpdateLog extends React.Component {
             Update Log
           </DialogTitle>
           <DialogContent>
-            <h3>Feb 25, 2019</h3>
+            <h4>Feb 25, 2019</h4>
             <p>Fixed a bug where alternate formes had the moveset of their base forme. For example, White Kyurem couldn't learn Fusion Flare (<a href='https://www.reddit.com/r/pokemon/comments/aumnvh/brand_new_ultra_sun_and_moon_team_builder/eh95wr3'>DMSivally</a>).</p>
             <p>Fixing the above bug caused selecting Megas to break the app. This is fixed too now (<a href='https://www.reddit.com/r/pokemon/comments/aumnvh/brand_new_ultra_sun_and_moon_team_builder/eha3o9p'>kwiszat</a>).</p>
-            <h3>Mar 6, 2019</h3>
+            <h4>Mar 6, 2019</h4>
             <p>Super effective STAB moves now count for +2 instead of +1.</p>
           </DialogContent>
           <DialogActions>
