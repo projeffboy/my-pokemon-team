22da2622171dd0be638d666845c1144cbbe8a7a9
Jeffery <jeffery124@gmail.com>
2019-03-09 21:35:22 -0500

    README, and link to github from site

---
+++ README.md
@@ -0,0 +1,31 @@
+# [My Pokemon Team](https://mypokemonteam.com)
+### The Ultimate Pokemon Teambuilder!
+
+![My Pokemon Team Screenshot](src/my-pokemon-team-screenshot.png)
+
+## Hiatus
+I have midterms and finals all the way to April :/
+
+## Features to Work on
+In the summer, I plan on implementing these 3 things first:
+1. Setting EV, IV, and natures
+2. Using your base speed, EV, and IV to determine your speed tier
+3. Polishing the design of the site
+
+I'll be sure to update the site for Pokemon Sword and Shield when it comes out (as soon as Showdown has the data for it!)
+
+You can check out the issues tab to see what other improvements I have in mind. Or you can create a new issue if you have suggestions of your own :)
+
+## Major Credits
+- [Pokemon Showdown](https://pokemonshowdown.com/): animated sprites, non-animated sprites, and all the pokemon data (thanks Zarel!)
+- [Javascript React framework](https://reactjs.org/)
+- [Material UI](https://material-ui.com/)
+- [MobX state management](https://mobx.js.org/)
+- [Google Analytics](https://support.google.com/analytics/answer/1008015?hl=en)
+
+## Minor Credits
+- [Landorus-T Face](https://archive.nyafuu.org/vp/last/50/34683395/)
+- [Incineroar Face]('https://thegamehaus.com/wolfe-glick-wins-sixth-regional-title-vgc-2018-charlotte-regional-championships-recap/2018/03/20/')
+- [Bulbapedia's Type Chart](https://bulbapedia.bulbagarden.net/wiki/Type)
+- [Non-table Type Chart](https://www.pinterest.ca/pin/307159637067301004/)
+- [Assigning each type a color](https://guiguilegui.wordpress.com/2016/05/23/pokemon-type-classifier-using-their-colors')
--- src/update-log.js
+++ src/update-log.js
@@ -7,6 +7,7 @@ import DialogContent from '@material-ui/core/DialogContent'
 import DialogTitle from '@material-ui/core/DialogTitle'
 import Typography from '@material-ui/core/Typography'
 import Link from '@material-ui/core/Link'
+import Folder from '@material-ui/icons/Folder'
 
 class UpdateLog extends React.Component {
   constructor(props) {
@@ -32,6 +33,17 @@ class UpdateLog extends React.Component {
             Update Log
           </DialogTitle>
           <DialogContent>
+            <div style={{display: 'flex', justifyContent: 'center'}}>
+              <Button
+                variant='outlined'
+                color='primary'
+                href='https://github.com/projeffboy/my-pokemon-team'
+                style={{margin: '10px 0 30px'}}
+              >
+                <Folder style={{marginRight: 5}} />
+                GitHub Repo
+              </Button>
+            </div>
             <Typography variant='subtitle2'>Feb 25, 2019</Typography>
             <Typography paragraph>Fixed a bug where alternate formes had the moveset of their base forme. For example, White Kyurem couldn't learn Fusion Flare (<Link style={{color: '#2196f3'}} variant='inherit' href='https://www.reddit.com/r/pokemon/comments/aumnvh/brand_new_ultra_sun_and_moon_team_builder/eh95wr3'>DMSivally</Link>).</Typography>
             <Typography paragraph>Fixing the above bug caused selecting Megas to break the app. This is fixed too now (<Link style={{color: '#2196f3'}} variant='inherit' href='https://www.reddit.com/r/pokemon/comments/aumnvh/brand_new_ultra_sun_and_moon_team_builder/eha3o9p'>kwiszat</Link>).</Typography>
@@ -42,9 +54,11 @@ class UpdateLog extends React.Component {
             <Typography paragraph>Included a manual page clarifying how to use this site.</Typography>
             <Typography variant='subtitle2'>Mar 9, 2019</Typography>
             <Typography paragraph>You can now click (as well as hover) over the types for more information. Good for phones.</Typography>
-            <Typography paragraph>There's now a typechart button!</Typography>
+            <Typography paragraph>There's now a type chart button!</Typography>
             <Typography paragraph>Fixed a bug where alolan-form pokemon had the movesets of their non-alolan forms (<Link style={{color: '#2196f3'}} variant='inherit' href='https://www.reddit.com/r/stunfisk/comments/az2f34/behold_the_ultimate_teambuilder/ei4t5g6'>DJdeMaster</Link>).</Typography>
             <Typography paragraph>The search filter VGC 2018 is updated to VGC 2019 (<Link style={{color: '#2196f3'}} variant='inherit' href='https://www.reddit.com/r/stunfisk/comments/az2f34/behold_the_ultimate_teambuilder/ei4xcwx'>Elmodipus</Link>).</Typography>
+            <Typography paragraph>Added the "superior" type chart (<Link style={{color: '#2196f3'}} variant='inherit' href='http://i.imgur.com/fylyCdC.png'>Bardock_RD</Link>).</Typography>
+            <Typography paragraph>The code is now open sourced (<Link style={{color: '#2196f3'}} variant='inherit' href='https://www.reddit.com/r/stunfisk/comments/az2f34/behold_the_ultimate_teambuilder/ei4yxo3'>Crescive_Delta</Link>)! </Typography>
           </DialogContent>
           <DialogActions>
             <Button onClick={this.toggleDialog} color='primary'>
