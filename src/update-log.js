d3a8aec3b46e6c74763ba672e986e761cb29555e
Jeffery Tang <jeffery124@gmail.com>
2022-11-20 23:27:23 -0500

    update log status

--- src/update-log.js
+++ src/update-log.js
@@ -34,7 +34,7 @@ class UpdateLog extends React.Component {
             Update Log
           </DialogTitle>
           <DialogContent>
-            <Typography paragraph><mark>I am looking to update the site for Scarlet/Violet, but I have school until Dec 5. I will try to find free time.</mark></Typography>
+            <Typography paragraph><mark>I have school until Dec 5. I am planning some major changes, but those may take place next year.</mark></Typography>
             <div style={{display: 'flex', justifyContent: 'center'}}>
               <Button
                 variant='outlined'
