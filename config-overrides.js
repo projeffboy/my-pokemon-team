b0f34eebe2981807c4b7f0dd8ea7b4a21f44b530
Jeffery Tang <jeffery124@gmail.com>
2025-12-05 16:21:27 -0500

    refactor: remove unused MobX rewire configuration and dependency

--- config-overrides.js
+++ config-overrides.js
@@ -1,13 +1,3 @@
 const { override, addDecoratorsLegacy } = require("customize-cra");
 
 module.exports = override(addDecoratorsLegacy());
-
-/*
-const rewireMobX = require('react-app-rewire-mobx');
-
-// config-overrides.js
-module.exports = function override(config, env) {
-  config = rewireMobX(config, env);
-  return config;
-}
-*/
