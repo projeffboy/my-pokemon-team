268dd4e0b2c6c25ea40e53bf26867e1bd92ede07
Jeffery Tang <jeffery124@gmail.com>
2025-12-22 00:37:56 -0500

    Refactor PokemonCards component into its own file

---
+++ src/components/main/PokemonCards.tsx
@@ -0,0 +1,33 @@
+import Grid from "@mui/material/Grid";
+import Paper from "@mui/material/Paper";
+import Pokemon from "./Pokemon";
+import TeamViewer from "./TeamViewer";
+
+interface PokemonCardsProps {
+  width: string;
+}
+
+function PokemonCards({ width }: PokemonCardsProps) {
+  if (width !== "xs" && width !== "sm" && width !== "md") {
+    // if viewport width >=960px
+    // Display 6 pokemon cards
+    return (
+      <>
+        {[0, 1, 2, 3, 4, 5].map(num => (
+          <Grid key={num} size={{ xs: 6 }}>
+            <Paper sx={{ p: 1 }}>
+              {/* teamIndex is the pokemon's team slot number - 1 */}
+              <Pokemon teamIndex={num} width={width} />
+            </Paper>
+          </Grid>
+        ))}
+      </>
+    );
+  } else {
+    // if viewport width less than 960px
+    // Display 1 or 2 pokemon cards
+    return <TeamViewer width={width} />;
+  }
+}
+
+export default PokemonCards;
