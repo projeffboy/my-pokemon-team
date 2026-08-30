04f4abd06ef0ad9f45a7eee5da0fe02a8156c493
Jeffery Tang <jeffery124@gmail.com>
2026-08-30 01:07:42 -0700

    refactor: wrap App component in StrictMode for improved development experience

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,11 @@
+import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./app";
 
 const rootEl = document.getElementById("root");
 if (!rootEl) throw new Error("Root element not found");
-createRoot(rootEl).render(<App />);
+createRoot(rootEl).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
+);
